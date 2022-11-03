683d3189b5dd7965e415e893498d999536e801d2
Stephen Nenke <stephen@youritteam.com.au>
2022-11-03 20:58:03 +1100

    Add project files.

---
+++ HowTo_DBLibrary/HowToDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HowTo_DBLibrary
+{
+    public class HowToDbContext : DbContext
+    {
+        //Add a default constructor if scaffolding is needed
+        public HowToDbContext() { }
+        //Add the complex constructor for allowing Dependency Injection
+        public HowToDbContext(DbContextOptions options)
+        : base(options)
+        {
+            //intentionally empty.
+        }
+    }
+}
